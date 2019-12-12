0650a578fb60200927d3b2a8e111568e86ccf4c4
sskywalker <hkrai39@gmail.com>
2019-12-12 17:59:25 +0530

    added routing functionality for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/hompage.component'
+import HomePage from './pages/homepage/hompage.component';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route path="/" exact={true} component={HomePage} />
+      </Switch>
     </div>
   );
 }
