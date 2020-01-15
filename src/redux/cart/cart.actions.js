871ceeb4d0532d2816ee63647290da65c93ba2fb
sskywalker <hkrai39@gmail.com>
2020-01-15 10:46:50 +0530

    updated cart item to group similar items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartAcionTypes from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CartAcionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+  type: CartAcionTypes.ADD_ITEM,
+  payload: item
+});
